152bbbe0891aed8536d50536773f62bb466a2dfc
OussamaBenoujja <osama2code79@gmail.com>
2025-04-08 11:13:10 +0100

    adding intial files

---
+++ src/components/navbar.js
@@ -0,0 +1,19 @@
+
+import React from "react";
+
+
+function Navbar() {
+  return (
+    <div className="navbar bg-dark text-white p-3">
+      <h1>Navbar</h1>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Navbar;
+// This is a simple Navbar component that displays a title and a list of links.
---
+++ src/pages/home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from '../components/navbar';
+
+
+
+function Home() {
+  return (
+    <div className="home">
+      <Navbar />
+      <h1>Home Page</h1>
+      <p>Welcome to the home page!</p>
+    </div>
+  );
+}
+
+export default Home;
